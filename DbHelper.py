e4337fe38030813cd91e0a654d2d07e018f9c893
gabe ban <gabe.banciu@gmail.com>
2023-02-19 21:57:43 -0800

    chunk pd read_csv, copy_from works

--- DbHelper.py
+++ DbHelper.py
@@ -19,56 +19,42 @@ class DbHelper:
         conn = psycopg2.connect(**DATABASE_PARAMS)
         self.create_and_load_test_table(conn)
 
-    def insert_with_string_io(self, df: pd.DataFrame, table_name: str, conn):
+
+    def create_and_load_test_table(self, db_connection):
+
+        def insert_with_string_io(df: pd.DataFrame, table_name: str):
             buffer = io.StringIO()
             df.to_csv(buffer, index=False, header=False, sep='\t' )
             buffer.seek(0)
-            with conn.cursor() as cursor:
+            with db_connection.cursor() as cursor:
                 try:
                     cursor.copy_from(file=buffer, table=table_name,  null="", sep='\t', columns=['vector'])
                 except (Exception, psycopg2.DatabaseError) as error:
                     print("Error: %s" % error)
 
-    def create_and_load_test_table(self, db_connection):
-        with db_connection.cursor() as cur:
-            # Load the remaining rows CSV file into a pandas DataFrame
-            df = pd.read_csv("test_data/remaining_rows.csv", header=None)
-            df = df.head(100)
+        def processChunk(df):
+            # do some data munging to turn float arrays into postgres array strings
             rows = []
             for row in df.itertuples(index=False):
                 rows.append(row)
             newdf = pd.DataFrame({'vector': rows})
             newdf['vector'] = newdf.vector.apply(lambda x: lst2pgarrexplicit(x))
-            print(newdf)
+            insert_with_string_io(newdf, 'csv_vectors')
+
+
+        with db_connection.cursor() as cur:
             cur.execute("CREATE EXTENSION IF NOT EXISTS cube;")
-            # Create a new table to store the remaining data
+            # Create an intermediate table table to store raw arrays
             cur.execute("CREATE TABLE IF NOT EXISTS csv_vectors (vector float[])")
-            self.insert_with_string_io(newdf, 'csv_vectors', db_connection)
-            db_connection.commit()
-            cur.close()
-
-            #cur.execute("CREATE TABLE IF NOT EXISTS vector_benchmarks (id serial PRIMARY KEY, vector integer[])")
-            #cur.execute("SELECT FROM")
-            # Insert the remaining data into the table
-            # We have create the sql inline, parameters dont pass the array correctly
-            #df = df.fillna(0)
-            #for row in df.itertuples(index=False):
-            #    cur.execute("INSERT INTO benchmark_vector (vector) VALUES (cube(ARRAY[%s]))" % lst2pgarr(row))
-            #    db_connection.commit()
-
-            # sql = """
-            #     INSERT INTO benchmark_vector (vector)
-            #     VALUES (cube(ARRAY%s)) 
-            # """
-            # first_3 = df.head(3)
-            # print(first_3)
-            # data_gen = []
-            # for index, row in first_3.iterrows():
-            #     data_gen.append(lst2pgarr(row))
-            # print(data_gen)
-            # psycopg2.extras.execute_batch(cur, sql, ((1,3,4), (1,234,3)), page_size=1000)
-
+            with pd.read_csv("test_data/remaining_rows.csv", chunksize=100, header=None) as reader:
+                for chunk in reader:
+                    processChunk(chunk)
+                    db_connection.commit()
             
+            # select insert from csv_vectors to cube column 
+            cur.execute("CREATE TABLE IF NOT EXISTS benchmark_vectors (id serial PRIMARY KEY, vector cube)")
+            cur.execute("INSERT INTO benchmark_vectors (vector) SELECT cube(vector) FROM csv_vectors")
+            cur.close()
 
 if __name__ == "__main__":
     helper = DbHelper()
